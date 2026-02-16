1d64737431a69484424d5ef452f79554218cfa48
GilTex23 <s.poltanov8@gmail.com>
2026-02-16 20:13:12 +1000

    Save .env loading

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 import os
 from dotenv import load_dotenv
+import logging
 
-load_dotenv()
+logger = logging.getLogger(__name__)
+try:
+    load_dotenv()
+    logger.info("The virtual environment is loaded")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [int(id_) for id_ in os.getenv("ADMIN_IDS", "").split(",") if id_]
-DATABASE_URL = os.getenv("DATABASE_URL")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    ADMIN_IDS = [int(id_) for id_ in os.getenv("ADMIN_IDS", "").split(",") if id_]
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
-api_keys_ = [key_.split('-') for key_ in [key_.strip() for key_ in os.getenv('SCRAPER_API_KEYS', '').split(',') if key_] if key_]
-if api_keys_:
-    SCRAPER_API_KEYS = api_keys_
-else:
-    raise ValueError("At least one argument of SCRAPER_API_KEYS is required. Format: NAME–API_KEY, NAME2–API_KEY, ...")
+    api_keys_ = [key_.split('-') for key_ in [key_.strip() for key_ in os.getenv('SCRAPER_API_KEYS', '').split(',') if key_] if key_]
+    if api_keys_:
+        SCRAPER_API_KEYS = api_keys_
+    else:
+        raise ValueError("At least one argument of SCRAPER_API_KEYS is required. Format: NAME–API_KEY, NAME2–API_KEY, ...")
+
+    logger.info("The virtual environment is installed")
+except Exception as e:
+    logger.error(f"Error env installing: {e}")
+    raise e
